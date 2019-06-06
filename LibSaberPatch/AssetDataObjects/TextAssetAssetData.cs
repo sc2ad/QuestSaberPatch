9d5cd7af6d52fc9029eb9656d1bae01e1940f5c9
leo60228 <iakornfeld@gmail.com>
2019-06-06 09:07:10 -0400

    Many optimizations

--- LibSaberPatch/AssetDataObjects/TextAssetAssetData.cs
+++ LibSaberPatch/AssetDataObjects/TextAssetAssetData.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace LibSaberPatch.AssetDataObjects
 {
-    public class TextAssetAssetData : AssetData
+    public sealed class TextAssetAssetData : AssetData
     {
         // Master Polyglot: c4dc0d059266d8d47862f46460cf8f31, 1
         // BeatSaber: 231368cb9c1d5dd43988f2a85226e7d7, 1
@@ -20,12 +20,12 @@ namespace LibSaberPatch.AssetDataObjects
             script = reader.ReadAlignedString();
         }
 
-        public override int SharedAssetsTypeIndex()
+        public sealed override int SharedAssetsTypeIndex()
         {
             return 0;
         }
 
-        public override void WriteTo(BinaryWriter w)
+        public sealed override void WriteTo(BinaryWriter w)
         {
             w.WriteAlignedString(name);
             w.WriteAlignedString(script);
